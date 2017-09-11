dfca35cecf44e3b985904bb6a3ac52c67f9b492e
Chujie Zeng <chujie.zeng@gmail.com>
2017-09-10 23:21:08 -0400

    Play with multi-layer nn

--- Proj_1/testMNISTmod.py
+++ Proj_1/testMNISTmod.py
@@ -7,17 +7,28 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 class Network(object):
     def __init__(self):
-        self.lr = 0.001
+        self.lr = 0.1
         self.batch_size = 100
-        self.max_iter = 5000
+        self.max_iter = 20000
+        self.hidden_layer = 128
+        self.hidden_layer_2 = 32
         self.build_graph()
         self.output_file = "output.csv"
 
     def build_graph(self):
         self.x = tf.placeholder(tf.float32, [None, 784])
-        W = tf.Variable(tf.zeros([784, 10]))
-        b = tf.Variable(tf.zeros([10]))
-        self.y = tf.nn.softmax(tf.matmul(self.x, W) + b)
+        W_1 = tf.Variable(tf.truncated_normal([784, self.hidden_layer], stddev=0.1))
+        b_1 = tf.Variable(tf.zeros([self.hidden_layer]))
+        hidden = tf.sigmoid(tf.matmul(self.x, W_1) + b_1)
+
+        W_2 = tf.Variable(tf.truncated_normal([self.hidden_layer, self.hidden_layer_2], stddev=0.1))
+        b_2 = tf.Variable(tf.zeros([self.hidden_layer_2]))
+        hidden_2 = tf.sigmoid(tf.matmul(hidden, W_2) + b_2)
+
+        W_output = tf.Variable(tf.truncated_normal([self.hidden_layer_2, 10], stddev=0.1))
+        b_output = tf.Variable(tf.zeros([10]))
+        self.y = tf.nn.softmax(tf.matmul(hidden_2, W_output) + b_output)
+
         self.y_ = tf.placeholder(tf.float32, [None, 10])
         self.cross_entropy = tf.reduce_mean(-tf.reduce_sum(self.y_ * tf.log(self.y), reduction_indices=[1]))
         self.train_step = tf.train.GradientDescentOptimizer(self.lr).minimize(self.cross_entropy)
@@ -37,12 +48,11 @@ class Network(object):
                 batch_xs, batch_ys = mnist.train.next_batch(self.batch_size)
                 self.sess.run(self.train_step, feed_dict={self.x: batch_xs, self.y_: batch_ys})
                 if i % 10 == 0:
-                    acc = self.sess.run(self.accuracy, feed_dict={self.x: mnist.test.images, self.y_: mnist.test.labels})
-                    loss = self.sess.run(self.cross_entropy, feed_dict={self.x: mnist.test.images, self.y_: mnist.test.labels})
+                    acc, loss = self.sess.run([self.accuracy, self.cross_entropy], feed_dict={self.x: mnist.test.images, self.y_: mnist.test.labels})
+                    # loss = self.sess.run(self.cross_entropy, feed_dict={self.x: mnist.test.images, self.y_: mnist.test.labels})
                     writer.writerow({'iter': i, 'loss': loss, 'acc': acc})
                     print("iter: {}, loss: {}, accuracy: {}".format(i, loss, acc))
-            acc = self.sess.run(self.accuracy, feed_dict={self.x: mnist.test.images, self.y_: mnist.test.labels})
-            # loss = self.sess.run(self.cross_entropy, feed_dict={self.x: mnist.test.images, self.y_: mnist.test.labels})
+            acc, loss = self.sess.run([self.accuracy, self.cross_entropy], feed_dict={self.x: mnist.test.images, self.y_: mnist.test.labels})
             writer.writerow({'iter': self.max_iter, 'loss': loss, 'acc': acc})
             print("iter: {}, loss: {}, accuracy: {}".format(self.max_iter, loss, acc))
 
